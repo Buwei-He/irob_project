509a4c858141b90929661a8b1b486fce0e7cf3a7
Buwei-He <11911219@mail.sustech.edu.cn>
2023-10-02 21:30:20 +0200

    Corner case detection and respawn model added.

--- scripts/state_machines/state_machine_test.py
+++ scripts/state_machines/state_machine_test.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy import linalg as LA
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose, Twist
 from std_srvs.srv import Empty, SetBool, SetBoolRequest  
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 from robotics_project.srv import MoveHead, MoveHeadRequest, MoveHeadResponse
@@ -15,6 +15,7 @@ from actionlib import SimpleActionClient
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from nav_msgs.msg import Odometry
 
+from gazebo_msgs.srv import SpawnModel, DeleteModel, DeleteModelRequest
 from moveit_msgs.msg import MoveItErrorCodes
 
 moveit_error_dict = {}
@@ -37,7 +38,9 @@ class StateMachine(object):
         
         self.node_name = "Student SM"
         self.aruco_pose_received = False
-        self.robot_pose = None
+        self.robot_pose_received = False
+        self.gripper_position = [1, 1]
+        self.gripper_effort = [0, 0]
         self.prev_state = 0
         self.state = 0
 
@@ -52,7 +55,9 @@ class StateMachine(object):
         # Localization and Costmap service
         self.global_loc_service_ns = rospy.get_param(rospy.get_name() + '/global_loc_srv')
         self.clear_costmaps_service_ns = rospy.get_param(rospy.get_name() + '/clear_costmaps_srv')
-        
+        # Reset aruco service
+        self.reset_aruco_service_ns = rospy.get_param(rospy.get_name() + '/reset_aruco_srv')
+
         # Pick/Place/Aruco pose topic
         self.pick_pose_topic_ns = rospy.get_param(rospy.get_name() + '/pick_pose_topic')
         self.place_pose_topic_ns = rospy.get_param(rospy.get_name() + '/place_pose_topic')
@@ -60,6 +65,8 @@ class StateMachine(object):
         # Command velocity and Navigation goal topic
         self.cmd_vel_topic_ns = rospy.get_param(rospy.get_name() + '/cmd_vel_topic')
         self.nav_goal_topic_ns = rospy.get_param(rospy.get_name() + '/nav_goal_topic')
+        # Joint states topic
+        self.joint_states_topic_ns = rospy.get_param(rospy.get_name() + '/joint_states_topic')
 
         print("\
             move_head_service: %s \n \
@@ -72,20 +79,24 @@ class StateMachine(object):
             aruco_pose_topic: %s \n \
             cmd_vel_topic: %s \n \
             nal_goal_topic: %s \n \
+            joint_states_topic: %s \n \
         " % (self.move_head_service_ns, self.pick_service_ns, self.place_service_ns,
             self.global_loc_service_ns, self.clear_costmaps_service_ns,
             self.pick_pose_topic_ns, self.place_pose_topic_ns, self.aruco_pose_topic_ns,
-            self.cmd_vel_topic_ns, self.nav_goal_topic_ns)
+            self.cmd_vel_topic_ns, self.nav_goal_topic_ns, self.joint_states_topic_ns)
         )
 
         # Subscribe to topics
         self.robot_pose_subs = rospy.Subscriber(self.global_loc_service_ns, PoseWithCovarianceStamped, self.robot_pose_callback)
         self.aruco_pose_subs = rospy.Subscriber(self.aruco_pose_topic_ns, PoseStamped, self.aruco_pose_callback)
+        self.joint_states_subs = rospy.Subscriber(self.joint_states_topic_ns, JointState, self.joint_states_callback)
+
 
         # Wait for service providers
         rospy.wait_for_service(self.move_head_service_ns, timeout=30)
         rospy.wait_for_service(self.pick_service_ns, timeout=30)
         rospy.wait_for_service(self.place_service_ns, timeout=30)
+        rospy.wait_for_service(self.reset_aruco_service_ns, timeout=30)
 
         # Instantiate publishers
         self.cmd_vel_pub = rospy.Publisher(self.cmd_vel_topic_ns, Twist, queue_size=10)
@@ -111,6 +122,8 @@ class StateMachine(object):
         self.check_states()
 
 
+    # callback
+
     def aruco_pose_callback(self, aruco_pose_msg):
         """ Update aruco pose. """
         valid = aruco_pose_msg is not None
@@ -123,7 +136,18 @@ class StateMachine(object):
         """ Update robot pose. """
         valid = robot_pose_msg is not None
         if valid:
-            self.robot_pose = robot_pose_msg
+            self.robot_pose_received = True
+        return valid
+
+
+    def joint_states_callback(self, joint_states_msg):
+        """ Update gripper joints state. """
+        valid = joint_states_msg is not None
+        if valid:
+            self.gripper_position = [joint_states_msg.position[7], joint_states_msg.position[8]]
+            self.gripper_effort = [joint_states_msg.effort[7], joint_states_msg.effort[8]]
+            if self.state == 6:
+                self.aruco_fall_detect()
         return valid
 
 
@@ -143,11 +167,17 @@ class StateMachine(object):
 
 
     def navigate_to_pose(self, target_pose):
-        rospy.loginfo("%s: Navigating to the target pose...", self.node_name)
-        goal = MoveBaseGoal()
-        goal.target_pose = target_pose
-        self.move_base_ac.send_goal(goal)
-        success = self.move_base_ac.wait_for_result(rospy.Duration(100.0))
+        try:
+            rospy.loginfo("%s: Navigating to the target pose...", self.node_name)
+            goal = MoveBaseGoal()
+            goal.target_pose = target_pose
+            self.move_base_ac.send_goal(goal)
+            self.move_base_ac.wait_for_result(rospy.Duration(100.0))
+            success = self.move_base_ac.get_state()
+        
+        except rospy.ROSException as e:
+            rospy.loginfo("Navigation error: %s", e)
+            success = False
 
         if success:
             rospy.loginfo("%s: Target pose reached.", self.node_name)
@@ -159,7 +189,6 @@ class StateMachine(object):
     
 
     def move_base_motion(self, move_msg, time):
-
         rate_per_sec = 10
         rate = rospy.Rate(rate_per_sec)
         cnt = 0
@@ -212,6 +241,24 @@ class StateMachine(object):
         return success
     
 
+    def aruco_fall_detect(self):
+        # gripper distance and effort (force)
+        disp_threshold = 0.02
+        force_threshold = 0.5
+        p_left, p_right = self.gripper_position[0], self.gripper_position[1]
+        f_left, f_right = self.gripper_effort[0], self.gripper_effort[1]
+        closed = (p_left < disp_threshold) and (p_right < disp_threshold)
+        force_reduced = (abs(f_left) < force_threshold) and (abs(f_right) < force_threshold)
+        if closed and force_reduced:
+            rospy.loginfo("%s: Aruco cube is thrown away!", self.node_name)
+            rospy.loginfo("%s: Position: %0.2f, %0.2f", self.node_name, p_left, p_right)
+            rospy.loginfo("%s: Effort: %0.2f, %0.2f", self.node_name, f_left, f_right)
+        # visual detect?
+            return False
+        
+        else: return True
+    
+
     def move_head_motion(self, direction="down"):
         try:
             rospy.loginfo("%s: Moving robot head", self.node_name)
@@ -230,6 +277,44 @@ class StateMachine(object):
             rospy.loginfo("Service call to move_head server failed: %s" % e)
             return False
     
+
+    def reset_aruco(self):
+
+        rospy.loginfo("%s: Reset aruco cube...", self.node_name)
+        aruco_cube_path = rospy.get_param("/gazebo_models_handler/aruco_cube_sdf")
+        self.dlt_model_srv = "/gazebo/delete_model"
+        self.spawn_model_srv = "/gazebo/spawn_sdf_model"
+
+        # Wait for service providers
+        rospy.wait_for_service(self.dlt_model_srv, timeout=30)
+        rospy.wait_for_service(self.spawn_model_srv, timeout=30)
+        self.delete_model_srv = rospy.ServiceProxy(self.dlt_model_srv, DeleteModel)
+
+        rospy.loginfo("%s: Services received!", self.node_name)
+
+        # Initial pose of the cube
+        initial_pose = Pose()
+        initial_pose.position.x = -1.130530
+        initial_pose.position.y = -6.653650
+        initial_pose.position.z = 0.862500
+
+        f = open(aruco_cube_path,'r')
+        sdffile = f.read()
+
+        rospy.loginfo("%s: Calling gazebo delete_models", self.node_name)
+        try:
+            delete_cube = DeleteModelRequest('aruco_cube')
+            delete_model_res = self.delete_model_srv(delete_cube)
+
+        except rospy.ServiceException as e:
+            print("Service call to gazebo delete_models failed: %s"%e)
+
+        rospy.loginfo("%s: Respawn aruco model...", self.node_name)
+        spawn_model_prox = rospy.ServiceProxy(self.spawn_model_srv, SpawnModel)
+        spawn_model_prox("aruco_cube", sdffile, "/", initial_pose, "world")
+        
+        return 0
+
 
     def check_states(self):
 
@@ -325,13 +410,15 @@ class StateMachine(object):
                     pick_service = rospy.ServiceProxy(self.pick_service_ns, SetBool)
                     pick_req = pick_service()
                     self.prev_state = self.state
+                    success = self.aruco_fall_detect()
 
-                    if pick_req.success:
+                    if pick_req.success and success:
                         self.state = 6
                         rospy.loginfo("%s: Pick up succeeded!", self.node_name)
                     else:
                         self.state = 2
                         rospy.loginfo("%s: Pick up failed!", self.node_name)
+                        self.reset_aruco()
 
                     rospy.sleep(3)
 
